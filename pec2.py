f877c10dacf702aa1b8ac1827841f0a009182376
eduardo <nglc.leon@gmail.com>
2018-11-12 17:55:48 -0500

    Subida de tarea 1.2

--- pec2.py
+++ pec2.py
@@ -13,8 +13,7 @@ def grabarArchivo(archivo,tablaProducto):
   return
 # funcion para consular productos
 def consultaDisponibilidad(url,palabraClave,tablaProducto):
-    #response = requests.get(url+palabraClave)
-
+    # iniciamos sesion en la pagina web
     session = requests.Session()
     session.post("http://buscador.tecnomega.com/sesion.php", data=dict(
         ruc="1718760612001",
@@ -23,27 +22,32 @@ def consultaDisponibilidad(url,palabraClave,tablaProducto):
     ))
     response = session.get(url+palabraClave)
     print(response.status_code)
-    #print(response.content)
     producto = []
     soup = BeautifulSoup(response.content,"html.parser")
     table = soup.findAll(bgcolor="#000000");
+    # Recorremos los tags que necesitamos para el dataset
     for row in table[1].findAll("tr"):
         if row.tr is not None:
             xx = row.tr.parent.parent.parent
             for xy in xx.findAll("td"):
                 if xy.tr is None and xy.string is not None:
                     try:
+                        # Eliminamos los espacios en blanco generados al inicio y final de cada atributo del articulo
                         producto.append(xy.string.strip())
                     except AttributeError as error:
+                        # En caso de error no se elimina ningun espacio
                         producto.append(xy.string)
+            # Se carga los datos en la tablaProducto
             tablaProducto.append(producto)
             producto = []
     return
-#Current directory where is located the script
+# Directorio del script
 dir_dataset = os.path.dirname(__file__)
 dataset = "dataset.csv"
 archivo_dataset = os.path.join(dir_dataset, dataset)
+# Url para hacer el raspado web.
 url="http://buscador.tecnomega.com/index.php?buscar="
+# referencia de articulo a buscar
 print("¿Producto que desea buscar?")
 palabraClave = input()
 tablaProducto = []
